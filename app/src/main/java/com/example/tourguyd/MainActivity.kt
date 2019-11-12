3ff539a9b66f8663a8ab9919f3db0df10ad715fe
christian <ch393156@dal.ca>
2019-11-11 20:52:50 -0400

    Commit Setup

--- app/src/main/java/com/example/tourguyd/MainActivity.kt
+++ app/src/main/java/com/example/tourguyd/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.tourguyd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navigation = MapboxNavigation(this, "@string/MAPBOX_ACCESS_TOKEN")
     }
 }
