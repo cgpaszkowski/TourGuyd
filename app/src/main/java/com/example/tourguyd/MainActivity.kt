5ba3a16ed8630bef302ba4cb517ecd48290111e5
christian <ch393156@dal.ca>
2019-11-12 01:59:34 -0400

    mapbox 1.0.0

--- app/src/main/java/com/example/tourguyd/MainActivity.kt
+++ app/src/main/java/com/example/tourguyd/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.tourguyd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navigation = MapboxNavigation(this, "@string/MAPBOX_ACCESS_TOKEN")
+
+        val navigation = MapboxNavigation(applicationContext, "@String/MAPBOX_ACCESS_TOKEN")
+        Mapbox.getInstance(applicationContext, getString(R.string.MAPBOX_ACCESS_TOKEN))
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+
     }
 }
