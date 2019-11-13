12abf1f9adaad4f8add7f493bfb50f096e4d3711
christian <ch393156@dal.ca>
2019-11-12 20:27:31 -0400

    mapbox 1.0.1: Map works (at preset coordinates in New Zealand)

--- app/src/main/java/com/example/tourguyd/MainActivity.kt
+++ app/src/main/java/com/example/tourguyd/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.tourguyd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +17,55 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Mapbox.getInstance(applicationContext, "@String/MAPBOX_ACCESS_CODE")
 
-        val navigation = MapboxNavigation(applicationContext, "@String/MAPBOX_ACCESS_TOKEN")
-        Mapbox.getInstance(applicationContext, getString(R.string.MAPBOX_ACCESS_TOKEN))
+        setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+            }
+        }
+
 
     }
+
+    public override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+
 }
