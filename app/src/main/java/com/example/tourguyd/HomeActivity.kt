eadb24e1fd69c20ac60fb2a5d849113d8cab507e
cgpaszkowski <polishstriker_12@live.ca>
2019-11-29 23:50:25 -0400

    TourGuy'd 1.0.7: Improved recyclerview with click listener to home activity

--- app/src/main/java/com/example/tourguyd/HomeActivity.kt
+++ app/src/main/java/com/example/tourguyd/HomeActivity.kt
@@ -3,35 +3,45 @@ package com.example.tourguyd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var tourAdapter: TourRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        initRecyclerView()
-        addDataSet()
-    }
-
-    private fun addDataSet(){
-        val data = DataSource.createDataSet()
-        tourAdapter.submitList(data)
-    }
-
-    private fun initRecyclerView(){
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(this@HomeActivity)
-            val topSpacingDec = TopSpacingItemDecoration(30)
-            addItemDecoration(topSpacingDec)
-            tourAdapter = TourRecyclerAdapter()
-            adapter = tourAdapter
-        }
+        rv_tours.layoutManager = LinearLayoutManager(this)
+        rv_tours.setHasFixedSize(true)
+        val testData = createTestData()
+        rv_tours.adapter = TourAdapter(testData, { tourItem : TourData -> tourItemClicked(tourItem) })
 
     }
+
+    private fun tourItemClicked(tourItem : TourData) {
+        Toast.makeText(this, "Clicked: ${tourItem.itemName}", Toast.LENGTH_LONG).show()
+    }
+
+    private fun createTestData(): List<TourData> {
+        val tourList = ArrayList<TourData>()
+
+        tourList.add(TourData("Parks", "Tour Description 1"))
+        tourList.add(TourData("Museums", "Tour Description 2"))
+        tourList.add(TourData("Landmarks", "Tour Description 3"))
+        tourList.add(TourData("Parks", "Tour Description 4"))
+        tourList.add(TourData("Must Sees", "Tour Description 5"))
+        tourList.add(TourData("Museums", "Tour Description 6"))
+        tourList.add(TourData("Landmarks", "Tour Description 7"))
+        tourList.add(TourData("Parks", "Tour Description 8"))
+        tourList.add(TourData("Museums", "Tour Description 9"))
+        tourList.add(TourData("Must Sees ", "Tour Description 10"))
+        return tourList
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/tourguyd/TourData.kt
@@ -0,0 +1,3 @@
+package com.example.tourguyd
+
+data class TourData (val category: String, val itemName: String)
